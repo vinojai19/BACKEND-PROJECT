19176ff8c930c934870980adac34c19dd5ec70a3
vinojai19 <vinodhinijai1912@gmail.com>
2025-09-07 00:32:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/BACKEND-PROJECT/',
   plugins: [react()],
-  base: "/BACKEND-PROJECT",
-})
+});
